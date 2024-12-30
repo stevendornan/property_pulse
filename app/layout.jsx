958b2b863daed9deee8922938a608b3af8938c99
steven dornan <steven.dornan93@gmail.com>
2024-12-30 18:23:47 +0100

    Implemented Navbar component

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/Navbar";
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
